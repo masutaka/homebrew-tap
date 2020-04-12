8cf9570544fd14ff1ec575ea65e7f3b71f04e7c7
Takashi Masuda <masutaka.net@gmail.com>
2020-04-13 00:07:56 +0900

    v4.1.4

--- github-nippou.rb
+++ github-nippou.rb
@@ -1,13 +1,13 @@
 class GithubNippou < Formula
   homepage 'https://github.com/masutaka/github-nippou'
-  version 'v4.1.3'
+  version 'v4.1.4'
 
   if Hardware::CPU.is_64_bit?
-    url 'https://github.com/masutaka/github-nippou/releases/download/v4.1.3/github-nippou_v4.1.3_darwin_amd64.zip'
-    sha256 'e5bbf62bd7eb80b84831db30ea6c9ea22e17d5a1e8a4eac99bcbdf53feacf70d'
+    url 'https://github.com/masutaka/github-nippou/releases/download/v4.1.4/github-nippou_v4.1.4_darwin_amd64.zip'
+    sha256 '2bb53572bded55609ec3f1d80e75f4bf5877233fb02716e555538acfe18fc10a'
   else
-    url 'https://github.com/masutaka/github-nippou/releases/download/v4.1.3/github-nippou_v4.1.3_darwin_386.zip'
-    sha256 'faeb918326ed839c407fa26ae0032ca8ece16ac4ae8ac8aba09d0492bf94462f'
+    url 'https://github.com/masutaka/github-nippou/releases/download/v4.1.4/github-nippou_v4.1.4_darwin_386.zip'
+    sha256 '2006cd8008d92f64a904866fe0ce85f99a9dc1eed53ed6222082465fe1ab95fb'
   end
 
   def install
