807fd519bfb3fbdc649a9d02802135962502b349
Takashi Masuda <masutaka.net@gmail.com>
2021-12-30 18:07:42 +0900

    Drop 386 binary from github-nippou v4.1.5
    See https://github.com/masutaka/github-nippou/releases/tag/v4.1.5

--- github-nippou.rb
+++ github-nippou.rb
@@ -1,14 +1,8 @@
 class GithubNippou < Formula
   homepage 'https://github.com/masutaka/github-nippou'
   version 'v4.1.4'
-
-  if Hardware::CPU.is_64_bit?
-    url 'https://github.com/masutaka/github-nippou/releases/download/v4.1.4/github-nippou_v4.1.4_darwin_amd64.zip'
-    sha256 '2bb53572bded55609ec3f1d80e75f4bf5877233fb02716e555538acfe18fc10a'
-  else
-    url 'https://github.com/masutaka/github-nippou/releases/download/v4.1.4/github-nippou_v4.1.4_darwin_386.zip'
-    sha256 '2006cd8008d92f64a904866fe0ce85f99a9dc1eed53ed6222082465fe1ab95fb'
-  end
+  url 'https://github.com/masutaka/github-nippou/releases/download/v4.1.4/github-nippou_v4.1.4_darwin_amd64.zip'
+  sha256 '2bb53572bded55609ec3f1d80e75f4bf5877233fb02716e555538acfe18fc10a'
 
   def install
     bin.install 'github-nippou'
