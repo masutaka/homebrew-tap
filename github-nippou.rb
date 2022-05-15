a022c559a3f2cdc997f9df9a9c12ef822527124d
Takashi Masuda <masutaka.net@gmail.com>
2022-05-16 00:19:37 +0900

    v4.1.8

--- github-nippou.rb
+++ github-nippou.rb
@@ -1,8 +1,8 @@
 class GithubNippou < Formula
   homepage 'https://github.com/masutaka/github-nippou'
-  version 'v4.1.7'
-  url 'https://github.com/masutaka/github-nippou/releases/download/v4.1.7/github-nippou_v4.1.7_darwin_amd64.zip'
-  sha256 'f96929d34b75f09699585865db8109944c1e97d75fb17c6004547e1eb995378a'
+  version 'v4.1.8'
+  url 'https://github.com/masutaka/github-nippou/releases/download/v4.1.8/github-nippou_v4.1.8_darwin_amd64.zip'
+  sha256 '348310ad64273ae297bd59f0ae96f0ea4ca6a35a5eb856191ae9284049d3017f'
 
   def install
     bin.install 'github-nippou'
