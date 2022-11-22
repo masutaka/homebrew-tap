e1f1b131c5bc46d4b2c4c817e74e773b2635c81a
Takashi Masuda <masutaka.net@gmail.com>
2022-11-22 16:22:48 +0900

    Support Linux

--- github-nippou.rb
+++ github-nippou.rb
@@ -1,8 +1,18 @@
 class GithubNippou < Formula
   homepage 'https://github.com/masutaka/github-nippou'
   version 'v4.1.11'
-  url 'https://github.com/masutaka/github-nippou/releases/download/v4.1.11/github-nippou_v4.1.11_darwin_amd64.zip'
-  sha256 '2fee4c7ab15c3c55bd69e00eedd9c123b8986c7d05c88fc73c3eb7a238b2a991'
+
+  on_macos do
+    url 'https://github.com/masutaka/github-nippou/releases/download/v4.1.11/github-nippou_v4.1.11_darwin_amd64.zip'
+    sha256 '2fee4c7ab15c3c55bd69e00eedd9c123b8986c7d05c88fc73c3eb7a238b2a991'
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url 'https://github.com/masutaka/github-nippou/releases/download/v4.1.11/github-nippou_v4.1.11_linux_amd64.zip'
+      sha256 'c8f0c178ac6b53b9d774d06b0a40bf363bb45f8090bbb90cb5cf6fa5024afbb8'
+    end
+  end
 
   def install
     bin.install 'github-nippou'
