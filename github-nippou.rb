af3b9fceec3d5506341ed1b879e03760a834881f
Takashi Masuda <masutaka.net@gmail.com>
2024-04-19 22:34:57 +0900

    v4.2.16

--- github-nippou.rb
+++ github-nippou.rb
@@ -1,28 +1,28 @@
 class GithubNippou < Formula
   desc "Print today's your GitHub activity for issues and pull requests"
   homepage 'https://github.com/masutaka/github-nippou'
-  version '4.2.15'
+  version '4.2.16'
   license 'MIT'
 
   on_macos do
     case
     when Hardware::CPU.arm?
-      url 'https://github.com/masutaka/github-nippou/releases/download/v4.2.15/github-nippou_v4.2.15_darwin_arm64.zip'
-      sha256 'e34a21efb5b2a13f975532e95ca7e3fc6155fcfc2cf9b979512b388ac4d0df25'
+      url 'https://github.com/masutaka/github-nippou/releases/download/v4.2.16/github-nippou_v4.2.16_darwin_arm64.zip'
+      sha256 '24f418d3bea64e9f5cd422ac0d6cc86fa60653485d7c021756ee08823fae20c2'
     when Hardware::CPU.intel?
-      url 'https://github.com/masutaka/github-nippou/releases/download/v4.2.15/github-nippou_v4.2.15_darwin_amd64.zip'
-      sha256 '1e2340702e247ad3385635aaeb60a8437f92cd6620e6ba62092dd8974d45e882'
+      url 'https://github.com/masutaka/github-nippou/releases/download/v4.2.16/github-nippou_v4.2.16_darwin_amd64.zip'
+      sha256 '868569719ce099971eeb353bd8aa2591663bf2b3d8d3690cd8a85b0b7cfff684'
     end
   end
 
   on_linux do
     case
     when Hardware::CPU.arm?
-      url 'https://github.com/masutaka/github-nippou/releases/download/v4.2.15/github-nippou_v4.2.15_linux_arm64.zip'
-      sha256 '49d4525a8e060c9293639a41b94091b1fafe2e82f6ba2cd390a937b9ee35b85f'
+      url 'https://github.com/masutaka/github-nippou/releases/download/v4.2.16/github-nippou_v4.2.16_linux_arm64.zip'
+      sha256 '50032001aa91330f096f818ce2b648fa0f2cba0810c6fd5765b66ca0dacb6a10'
     when Hardware::CPU.intel?
-      url 'https://github.com/masutaka/github-nippou/releases/download/v4.2.15/github-nippou_v4.2.15_linux_amd64.zip'
-      sha256 'b0ef0f88e5c690ec987183c095d7bb118aee4775a1876a0ade89489ff32f5e3c'
+      url 'https://github.com/masutaka/github-nippou/releases/download/v4.2.16/github-nippou_v4.2.16_linux_amd64.zip'
+      sha256 'b8ec163d4a5fb4be23a6f4ab7bb487e8bfbf6b6ff1e49a006afb55bd7260faff'
     end
   end
 
